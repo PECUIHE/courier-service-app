166cd8afc904bab8d44ef0b067594ab4b849404d
peculiar <pecuiheubammadu1st@gmail.com>
2025-02-19 05:05:14 +0100

    fix error

--- app/auth/resetPassword/page.tsx
+++ app/auth/resetPassword/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { Suspense, useState } from 'react';
+import { Suspense, useState } from 'react';
 import { updateUser } from './actions';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Loading = () => (
   <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
     <div className="text-white text-xl font-semibold">Loading...</div>
